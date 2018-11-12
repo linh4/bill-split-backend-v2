e3094b86a4c410d1a3392d5a1756123a2c6d63ea
linh4 <linh44h@gmail.com>
2018-11-11 23:07:16 -0500

    updated i-p

--- app/controllers/item_payers_controller.rb
+++ app/controllers/item_payers_controller.rb
@@ -1,5 +1,6 @@
 class ItemPayersController < ApplicationController
-
+  skip_before_action :authorized
+  
   def create
     params[:payer_id].each do |id|
       @itemPayer = ItemPayer.create(item_id: params[:item_id], payer_id: id.to_i)
