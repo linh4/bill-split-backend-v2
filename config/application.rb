cb31698dfba31c00371bc1b35c21bc7b6687680c
linh4 <linh44h@gmail.com>
2018-11-13 21:44:40 -0500

    updated cors

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,25 @@ module BillSplitBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    use Rack::Cors do
+  allow do
+    origins 'localhost:3000', '127.0.0.1:3000',
+            /\Ahttp:\/\/192\.168\.0\.\d{1,3}(:\d+)?\z/
+            # regular expressions can be used here
+
+    resource '/file/list_all/', :headers => 'x-domain-token'
+    resource '/file/at/*',
+        methods: [:get, :post, :delete, :put, :patch, :options, :head],
+        headers: 'x-domain-token',
+        expose: ['Some-Custom-Response-Header'],
+        max_age: 600
+        # headers to expose
+  end
+
+  allow do
+    origins '*'
+    resource '/public/*', headers: :any, methods: :get
+  end
+end
   end
 end
